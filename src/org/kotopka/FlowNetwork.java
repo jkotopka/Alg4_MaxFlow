65a799de7afd6749c697cb19034cbce74f906f5d
John Kotopka <jkotopka@gmail.com>
2021-03-30 10:39:58 -0500

    implement FlowNetwork.java

--- src/org/kotopka/FlowNetwork.java
+++ src/org/kotopka/FlowNetwork.java
@@ -1,4 +1,31 @@
 package org.kotopka;
 
 public class FlowNetwork {
+
+    private final int V;
+    private final Bag<FlowEdge>[] adj;
+
+    @SuppressWarnings("unchecked")
+    public FlowNetwork(int V) {
+        this.V = V;
+        this.adj = (Bag<FlowEdge>[]) new Bag[V];
+
+        for (int i = 0; i < adj.length; i++) {
+            adj[i] = new Bag<>();
+        }
+    }
+
+    public void addEdge(FlowEdge e) {
+        int v = e.from();
+        int w = e.to();
+        adj[v].add(e);
+        adj[w].add(e);
+    }
+
+    public Iterable<FlowEdge> adj(int v) {
+        if (v < 0 || v >= adj.length) throw new IllegalArgumentException("Invalid vertex: " + v);
+
+        return adj[v];
+    }
+
 }
