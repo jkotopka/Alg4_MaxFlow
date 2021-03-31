882c3142e0ec71d0f5322ee26e843450a67f5fe0
John Kotopka <jkotopka@gmail.com>
2021-03-31 10:43:48 -0500

    update FordFulkerson.java add test case for flow network on p. 887

--- src/org/kotopka/FordFulkerson.java
+++ src/org/kotopka/FordFulkerson.java
@@ -2,16 +2,14 @@ package org.kotopka;
 
 public class FordFulkerson {
 
-    private final boolean[] marked;
-    private final FlowEdge[] edgeTo;
+    private boolean[] marked;
+    private FlowEdge[] edgeTo;
     private double value;
 
     public FordFulkerson(FlowNetwork G, int s, int t) {
         int size = G.V();
         if (s < 0 || t < 0 || s >= size || t >= size) throw new IllegalArgumentException("Invalid network: " + s + "->" + t);
 
-        this.marked = new boolean[size];
-        this.edgeTo = new FlowEdge[size];
         this.value = 0.0;
 
         while (hasAugmentingPath(G, s, t)) {
@@ -32,6 +30,10 @@ public class FordFulkerson {
     }
 
     public boolean hasAugmentingPath(FlowNetwork G, int s, int t) {
+        // these two arrays have to be created here
+        marked = new boolean[G.V()];
+        edgeTo = new FlowEdge[G.V()];
+
         Queue<Integer> q = new Queue<>();
 
         q.enqueue(s);
@@ -62,6 +64,23 @@ public class FordFulkerson {
         if (v < 0 || v >= marked.length) throw new IllegalArgumentException("Invalid vertex: " + v);
 
         return marked[v];
+    }
+
+    public static void main(String[] args) {
+        FlowNetwork fn = new FlowNetwork(6);
+
+        fn.addEdge(new FlowEdge(0, 1, 2.0));
+        fn.addEdge(new FlowEdge(0, 2, 3.0));
+        fn.addEdge(new FlowEdge(1, 3, 3.0));
+        fn.addEdge(new FlowEdge(1, 4, 1.0));
+        fn.addEdge(new FlowEdge(2, 3, 1.0));
+        fn.addEdge(new FlowEdge(2, 4, 1.0));
+        fn.addEdge(new FlowEdge(3, 5, 2.0));
+        fn.addEdge(new FlowEdge(4, 5, 3.0));
+
+        FordFulkerson ff = new FordFulkerson(fn, 0, 5);
+
+        System.out.println("Flow value: " + ff.value());
     }
 
 }
